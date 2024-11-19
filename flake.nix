5bb15cb7ff8ebc0a3d43e98bb0eb5f0152a1a7d4
Kenichi Kamiya <kachick1@gmail.com>
2024-11-19 11:26:28 +0900

    Remove flake-utils and nixpkgs-fmt dependencies

--- flake.nix
+++ flake.nix
@@ -6,36 +6,49 @@
     # How to update the revision
     #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        # https://discourse.nixos.org/t/mark-a-devshell-dependency-as-insecure/24354/3
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = with pkgs;
-          mkShell {
-            buildInputs = [
-              # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
-              # https://github.com/kachick/dotfiles/pull/228
-              bashInteractive
+  outputs =
+    { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+      # List: https://github.com/NixOS/nixpkgs/blob/nixos-24.05/lib/systems/flake-systems.nix
+      #
+      # https://github.com/NixOS/nixpkgs/blob/475556854559746466df20d74eef189373816b67/flake.nix?plain=1#L11
+      # https://github.com/NixOS/nixpkgs/blob/475556854559746466df20d74eef189373816b67/lib/systems/default.nix?plain=1#L48-L56
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default =
+            with pkgs;
+            mkShellNoCC {
+              buildInputs = [
+                # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
+                # https://github.com/kachick/dotfiles/pull/228
+                bashInteractive
 
-              nodejs_20
-              deno
-              dprint
-              cargo-make
-              nil
-              nixpkgs-fmt
-              typos
+                nodejs_20
+                deno
+                dprint
+                cargo-make
+                nil
+                nixfmt-rfc-style
+                typos
 
-              # NOTE: Do NOT add actionlint as a dependency
-              # - It does not target actions; it lints the user's side.
-              # - It assumes major actions in a stable state, often causing problems between versions.
-              # - Use https://github.com/github/vscode-github-actions for a better solution to get hints.
-            ];
-          };
-      });
+                # NOTE: Do NOT add actionlint as a dependency
+                # - It does not target actions; it lints the user's side.
+                # - It assumes major actions in a stable state, often causing problems between versions.
+                # - Use https://github.com/github/vscode-github-actions for a better solution to get hints.
+              ];
+            };
+        }
+      );
+    };
 }
